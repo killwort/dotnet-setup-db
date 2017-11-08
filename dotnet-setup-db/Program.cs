bab2f028eef667b6e0863be4e656ad9336fbc677
Andrey Zakharov <killwort@gmail.com>
2017-11-08 10:28:55 +0300

    Downloaded nuget files now may be put in custom folder.

--- dotnet-setup-db/Program.cs
+++ dotnet-setup-db/Program.cs
@@ -17,9 +17,6 @@ using Newtonsoft.Json.Linq;
 
 namespace dotnet_setup_db
 {
-
-    
-
     class Program
     {
         static void Help(string error)
@@ -47,11 +44,13 @@ nocreate                           [Optional]  Do not create new database before
 transaction         tx             [Optional]  Wrap script into transaction. This is default.
 notransaction       notx           [Optional]  Do not wrap script into transaction.
 drop                               [Optional]  Drop database specified with agrument. This options prevents script from running.
+pkgpath             pkg, p         [Optional]  Path to directory to use for nuget package storage. Defaults to .pkg, needed only when -nuget is used.
 ");
         }
+
         static int Main(string[] args)
         {
-            string assembly = null, nuget = null, type = null, cstring = null, script = "create.sql", drop = null;
+            string assembly = null, nuget = null, type = null, cstring = null, script = "create.sql", drop = null, pkgPath = ".pkg";
             bool create = true, tx = true;
             for (var i = 0; i < args.Length; i++)
             {
@@ -86,7 +85,8 @@ drop                               [Optional]  Drop database specified with agru
                             return -2;
                         }
                         type = args[i + 1];
-                        i++; break;
+                        i++;
+                        break;
                     case "drop":
                         if (args.Length == i + 1)
                         {
@@ -94,7 +94,20 @@ drop                               [Optional]  Drop database specified with agru
                             return -2;
                         }
                         drop = args[i + 1];
-                        i++; break;
+                        i++;
+                        break;
+                    case "pkgpath":
+                    case "pkg":
+                    case "p":
+                    case "pkg-path":
+                        if (args.Length == i + 1)
+                        {
+                            Help($"You must specify directory after {args[i]} parameter");
+                            return -2;
+                        }
+                        pkgPath = args[i + 1];
+                        i++;
+                        break;
                     case "s":
                     case "c":
                     case "connect":
@@ -107,7 +120,8 @@ drop                               [Optional]  Drop database specified with agru
                             return -2;
                         }
                         cstring = args[i + 1];
-                        i++; break;
+                        i++;
+                        break;
                     case "script":
                         if (args.Length == i + 1)
                         {
@@ -115,7 +129,8 @@ drop                               [Optional]  Drop database specified with agru
                             return -2;
                         }
                         script = args[i + 1];
-                        i++; break;
+                        i++;
+                        break;
                     case "create":
                         create = true;
                         break;
@@ -134,7 +149,7 @@ drop                               [Optional]  Drop database specified with agru
                         tx = false;
                         break;
                     default:
-                        var kval = args[i].Split(new[] { '=' }, 2);
+                        var kval = args[i].Split(new[] {'='}, 2);
                         if (kval.Length != 2)
                         {
                             Help("Unknown option " + args[i]);
@@ -143,15 +158,24 @@ drop                               [Optional]  Drop database specified with agru
                         break;
                 }
             }
+
+            if (!Directory.Exists(pkgPath))
+                Directory.CreateDirectory(pkgPath);
+
             if (!string.IsNullOrEmpty(nuget))
             {
-                var rv = ResolveNugetPackage(nuget, null);
+                var rv = ResolveNugetPackage(nuget, null, pkgPath);
                 rv.Wait();
                 assembly = rv.Result;
             }
             if (string.IsNullOrEmpty(assembly))
             {
                 Help("Assembly reference must be specified.");
+                return -2;
+            }
+            if (!File.Exists(assembly))
+            {
+                Help("Assembly file does not exist.");
                 return -2;
             }
             if (string.IsNullOrEmpty(type))
@@ -164,16 +188,21 @@ drop                               [Optional]  Drop database specified with agru
                 Help("Connection string must be specified.");
                 return -2;
             }
+            if (!File.Exists(script))
+            {
+                Help($"Script file {script} does not exist.");
+                return -4;
+            }
             AppDomain.CurrentDomain.AssemblyResolve += (sender, evArgs) =>
             {
-                var f = Path.GetFullPath(Path.Combine(".pkg", new AssemblyName(evArgs.Name).Name + ".dll"));
-                if(File.Exists(f))
+                var f = Path.GetFullPath(Path.Combine(pkgPath, new AssemblyName(evArgs.Name).Name + ".dll"));
+                if (File.Exists(f))
                     return Assembly.LoadFile(f);
                 return null;
             };
             Console.Error.WriteLine("Creating driver instance");
             var asm = Assembly.LoadFile(Path.GetFullPath(assembly));
-            var drv=asm.GetType(type);
+            var drv = asm.GetType(type);
             using (var cnn = (IDbConnection) Activator.CreateInstance(drv))
             {
                 cnn.ConnectionString = cstring;
@@ -225,10 +254,10 @@ drop                               [Optional]  Drop database specified with agru
             return 0;
         }
 
-        private static async Task<string> ResolveNugetPackage(string nuget,string ver)
+        private static async Task<string> ResolveNugetPackage(string nuget, string ver, string pkgPath)
         {
             Console.Error.WriteLine($"Resolving package {nuget} {ver}");
-            var dlNuspecName = $".pkg\\{nuget}.{ver}.nuspec";
+            var dlNuspecName = Path.Combine(pkgPath, $"{nuget}.{ver}.nuspec");
             byte[] nuspecData;
             string nugetUrl = null;
             var wc = new WebClient();
@@ -244,7 +273,7 @@ drop                               [Optional]  Drop database specified with agru
                         ?.Cast<JObject>().OrderByDescending(x =>
                         {
                             var xversion = x["catalogEntry"]?["version"].Value<string>();
-                            if(xversion==null)return new Version();
+                            if (xversion == null) return new Version();
                             return new Version(xversion.Split('-')[0]);
                         })
                         .FirstOrDefault();
@@ -280,26 +309,26 @@ drop                               [Optional]  Drop database specified with agru
             }
             var refs = nuspec.Descendants(XName.Get("reference", nugetNs)).Select(x => x.Attribute("file")?.Value).Where(x => x != null).ToArray();
             if (!refs.Any()) refs = new[] {nuget + ".dll"};
-            if (!refs.All(x => File.Exists($".pkg\\{x}")))
+            if (!refs.All(x => File.Exists(Path.Combine(pkgPath, x))))
             {
                 Console.Error.WriteLine($"Downloading package {nuget}");
                 var nupkg = new ZipArchive(new MemoryStream(await wc.DownloadDataTaskAsync(nugetUrl)));
                 var mydll = nupkg.Entries.First(x => (x.FullName.StartsWith("lib/netstandard") || x.FullName.StartsWith("lib/netcoreapp")) && x.Name.EndsWith(".dll"));
                 if (!Directory.Exists(Path.GetDirectoryName(dlNuspecName)))
                     Directory.CreateDirectory(Path.GetDirectoryName(dlNuspecName));
-                using (var fs = File.Create($".pkg\\{mydll.Name}"))
+                using (var fs = File.Create(Path.Combine(pkgPath,mydll.Name)))
                     mydll.Open().CopyTo(fs);
             }
-            var deps = nuspec.Descendants(XName.Get("dependencies",nugetNs));
-            var groups=deps.SelectMany(x => x.Elements(XName.Get("group",nugetNs)))
+            var deps = nuspec.Descendants(XName.Get("dependencies", nugetNs));
+            var groups = deps.SelectMany(x => x.Elements(XName.Get("group", nugetNs)))
                 .Where(x => !(x.Attribute(XName.Get("targetFramework"))?.Value.StartsWith(".NETStandard") ?? false));
             var d = groups
                 .SelectMany(x => x.Elements(XName.Get("dependency", nugetNs)));
-            var dd=d
+            var dd = d
                 .Select(x => new {Elem = x, Id = x.Attribute(XName.Get("id"))?.Value})
                 .Where(x => !x.Id.StartsWith("System.") && x.Id != "NETStandard.Library" && x.Id != "Microsoft.NETCore.App");
-            Task.WaitAll(dd.Select(x => ResolveNugetPackage(x.Id, x.Elem.Attribute(XName.Get("version"))?.Value)).Cast<Task>().ToArray());
-            return $".pkg/{refs.First()}";
+            Task.WaitAll(dd.Select(x => ResolveNugetPackage(x.Id, x.Elem.Attribute(XName.Get("version"))?.Value, pkgPath)).Cast<Task>().ToArray());
+            return Path.Combine(pkgPath, refs.First());
         }
     }
 }
